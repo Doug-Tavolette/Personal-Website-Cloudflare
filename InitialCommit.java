a8ad7e72714cb849f01dcdbbd15cece04991c7b3
Doug-Tavolette <dft8437@rit.edu>
2025-05-28 22:25:24 -0400

    created react app

--- InitialCommit.java
+++ InitialCommit.java
@@ -1,3 +1,7 @@
-// public static void main(String[] args) {
-//     System.out.println("");
-// }
+
+
+public class InitialCommit{
+    public static void main(String[] args) {
+        System.out.println("1");
+    }
+}
